1e511a07197c87786da230a0355eb889abd2d013
joisaac <joisaac@pm.me>
2020-12-09 09:46:09 -0400

    Medium Python Exercises

--- medium-python.py
+++ medium-python.py
@@ -1,3 +1,22 @@
 # Write a function
 def is_leap(year):
     return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+
+# Minion game
+def minion_game(string):
+    s = 0
+    k = 0
+
+    for i in range(len(string)):
+        if string[i] in "AEIOU":
+            k += len(string) - i
+        else:
+            s += len(string) - i
+
+    if s > k:
+        print(f"Stuart {s}")
+    elif k > s:
+        print(f"Kevin {k}")
+    else:
+        print("Draw")
+
